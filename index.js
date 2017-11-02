103fd8e28138900dce8a1bc02a6ed0457dba59c6
boybutcher <BYRONdeLARGE@yahoo.com>
2017-11-02 11:36:36 -0700

    basic board render

--- index.js
+++ index.js
@@ -1,29 +1,42 @@
 const boardArray = [
-  [ , , , , , , , ],
-  [ , , , , , , , ],
-  [ , , , , , , , ],
-  [ , , , , , , , ],
-  [ , , , , , , , ],
-  [ , , , , , , , ],
-  [ , , , , , , , ],
-  [ , , , , , , , ],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
 ];
 
 const body = document.body;
 
 const board = document.createElement(`table`);
 
-const createRow = () => {
+const createCell = (index) => {
+  console.log(`creating cells`)
+  const cell = document.createElement(`td`);
+  cell.className = index % 2 === 0 ? `dark` : `light`;
+  return cell;
+}
+
+const createRow = (rowArray, index) => {
   const row = document.createElement(`tr`);
-  row.innerHTML = `row`;
+  row.className = index % 2 === 0 ? `even` : `odd`;
+  rowArray.forEach((cell, cellIndex) => {
+    if (row.className === `even`) {
+      row.appendChild(createCell(cellIndex));
+    } else {
+      row.appendChild(createCell(cellIndex + 1));
+    }
+  })
   return row;
 }
 
 const appendRows = () => {
-  boardArray.map(row => {
-    board.appendChild(createRow());
+  boardArray.forEach((row, index) => {
+    board.appendChild(createRow(row, index));
   })
-  return board;
 };
 
 appendRows();
